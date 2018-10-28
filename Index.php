b5b552988a3617daccc2774b545e0355959d37cb
X-COD3TEAM <44341514+X-COD3TEAM@users.noreply.github.com>
2018-10-29 06:24:34 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,80 @@
+<html>
+<title>Hacked by Mr.Jack _ 301</title>
+<meta property="og:image" content="https://data.whicdn.com/images/7027435/large.png"/>
+	<meta property="og:title"content="Mr.jack_301""/>
+	<meta property="og:description"content="Mr.Jack_301 Hacker ">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700,900'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+ <link href="https://fonts.googleapis.com/css?family=VT323|New+Rocker" rel="stylesheet">
+ <script> alert('Sory admin haacked by: Mr.JAc')
+ </script> 
+      <style>
+      html,
+body,
+.wrapper {
+  height: 100%;
+}
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font: 1rem / 1.516 'Montserrat', Arial, sans-serif;
+}
+
+.wrapper {
+  position: relative;
+  background: url(https://data.whicdn.com/images/7027435/large.png) no-repeat center center/cover;
+}
+.wrapper:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(33, 33, 33, 0.25);
+}
+
+header,
+.content,
+footer {
+  position: absolute;
+  }
+
+.content {
+  top: 50%;
+  left: 50%;
+  text-align: center;
+  color: #FFFFFF;
+  -webkit-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+}
+  .asu span{
+  font-size:10px;
+  }
+    </style>
+    
+</head>
+
+<body translate="no" >
+
+  <div class="wrapper">
+
+  <main class="content">
+   
+    <center>
+			<font face='New Rocker'size='10'font style='color:#444;text-align: center;text-shadow: 0 0 3px #FF4700, 0px 0px 5px #FF0000,0 0 5px #FF4700,0 0 5px #FF0000;'>	
+		<font face="New Rocker"> Mr.Jack_301</font></font>
+    <div class="asu">
+    	<span><p>Website Hacked By Mr.Jack_301 Follow My Social Media Account</span></p>
+    <center>
+	<i class="fas fa-envelope fa-2x"></i>
+		<i class="fab fa-instagram fa-2x"></i>
+		<i class="fab fa-facebook fa-2x"></i>
+		<i class="fab fa-watsapp fa-2x"></i>
+</html>
